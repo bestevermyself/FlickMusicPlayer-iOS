6d3eb82c03187446b37dcf431574e8926bd1242b
Shuhei Hasegawa <shuhei@bestevermyself.be>
2017-02-05 09:42:19 +0900

    Initial Commit

---
+++ FlickMusicPlayer-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickMusicPlayer-iOS
+//
+//  Created by Shuhei Hasegawa on 2017/02/05.
+//  Copyright © 2017年 Shuhei Hasegawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
